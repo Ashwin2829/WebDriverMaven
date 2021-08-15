1b4499bc4ed398cf6b12f956fa98da7f9241840d
ashwin <ashwin.karkera28@gmail.com>
2021-08-15 19:40:56 +0530

    intial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setup()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		
+	}
+	@Test
+	public void doLogin() {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"))).sendKeys("duhhj");
+		//driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("sshshs");
+	}
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+}
